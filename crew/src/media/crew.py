c8a01f08a6339a80e49b90f9cd8300a6ee44ed30
Yani <mezianiyani0@gmail.com>
2025-03-02 00:30:54 -0500

    LAST PLS

--- crew/src/media/crew.py
+++ crew/src/media/crew.py
@@ -1,27 +1,26 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 
-from dotenv import load_dotenv
-load_dotenv
-
-# Uncomment the following line to use an example of a custom tool
-# from docker_crew_template.tools.custom_tool import MyCustomTool
-
-# Check our tools documentations for more information on how to use them
-# from crewai_tools import SerperDevTool
+# If you want to run a snippet of code before or after the crew starts, 
+# you can use the @before_kickoff and @after_kickoff decorators
+# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
 
 @CrewBase
-class DockerCrewTemplate():
-	"""DockerCrewTemplate crew"""
+class Media():
+	"""Media crew"""
 
+	# Learn more about YAML configuration files here:
+	# Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
+	# Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
 	agents_config = 'config/agents.yaml'
 	tasks_config = 'config/tasks.yaml'
 
+	# If you would like to add tools to your agents, you can learn more about it here:
+	# https://docs.crewai.com/concepts/agents#agent-tools
 	@agent
 	def researcher(self) -> Agent:
 		return Agent(
 			config=self.agents_config['researcher'],
-			# tools=[MyCustomTool()], # Example of custom tool, loaded on the beginning of file
 			verbose=True
 		)
 
@@ -32,6 +31,9 @@ class DockerCrewTemplate():
 			verbose=True
 		)
 
+	# To learn more about structured task outputs, 
+	# task dependencies, and task callbacks, check out the documentation:
+	# https://docs.crewai.com/concepts/tasks#overview-of-a-task
 	@task
 	def research_task(self) -> Task:
 		return Task(
@@ -47,7 +49,10 @@ class DockerCrewTemplate():
 
 	@crew
 	def crew(self) -> Crew:
-		"""Creates the DockerCrewTemplate crew"""
+		"""Creates the Media crew"""
+		# To learn how to add knowledge sources to your crew, check out the documentation:
+		# https://docs.crewai.com/concepts/knowledge#what-is-knowledge
+
 		return Crew(
 			agents=self.agents, # Automatically created by the @agent decorator
 			tasks=self.tasks, # Automatically created by the @task decorator
--- crew/src/media/main.py
+++ crew/src/media/main.py
@@ -1,11 +1,10 @@
-# main.py 
-
 #!/usr/bin/env python
 import sys
 import warnings
 
-from docker_crew_template.crew import DockerCrewTemplate
-# from crew import DockerCrewTemplate 
+from datetime import datetime
+
+from media.crew import Media
 
 warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
 
@@ -19,9 +18,14 @@ def run():
     Run the crew.
     """
     inputs = {
-        'topic': 'AI LLMs'
+        'topic': 'AI LLMs',
+        'current_year': str(datetime.now().year)
     }
-    DockerCrewTemplate().crew().kickoff(inputs=inputs)
+    
+    try:
+        Media().crew().kickoff(inputs=inputs)
+    except Exception as e:
+        raise Exception(f"An error occurred while running the crew: {e}")
 
 
 def train():
@@ -32,7 +36,7 @@ def train():
         "topic": "AI LLMs"
     }
     try:
-        DockerCrewTemplate().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+        Media().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
 
     except Exception as e:
         raise Exception(f"An error occurred while training the crew: {e}")
@@ -42,7 +46,7 @@ def replay():
     Replay the crew execution from a specific task.
     """
     try:
-        DockerCrewTemplate().crew().replay(task_id=sys.argv[1])
+        Media().crew().replay(task_id=sys.argv[1])
 
     except Exception as e:
         raise Exception(f"An error occurred while replaying the crew: {e}")
@@ -55,85 +59,7 @@ def test():
         "topic": "AI LLMs"
     }
     try:
-        DockerCrewTemplate().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
+        Media().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
 
     except Exception as e:
-        raise Exception(f"An error occurred while replaying the crew: {e}")
-
-# main.py 
-
-#!/usr/bin/env python
-import sys
-import warnings
-
-from docker_crew_template.crew import DockerCrewTemplate
-# from crew import DockerCrewTemplate 
-
-warnings.filterwarnings("ignore", category=SyntaxWarning, module="pysbd")
-
-# This main file is intended to be a way for you to run your
-# crew locally, so refrain from adding unnecessary logic into this file.
-# Replace with inputs you want to test with, it will automatically
-# interpolate any tasks and agents information
-
-def run():
-    """
-    Run the crew.
-    """
-    inputs = {
-        'topic': 'AI LLMs'
-    }
-    DockerCrewTemplate().crew().kickoff(inputs=inputs)
-
-
-def train():
-    """
-    Train the crew for a given number of iterations.
-    """
-    inputs = {
-        "topic": "AI LLMs"
-    }
-    try:
-        DockerCrewTemplate().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while training the crew: {e}")
-
-def replay():
-    """
-    Replay the crew execution from a specific task.
-    """
-    try:
-        DockerCrewTemplate().crew().replay(task_id=sys.argv[1])
-
-    except Exception as e:
-        raise Exception(f"An error occurred while replaying the crew: {e}")
-
-def test():
-    """
-    Test the crew execution and returns the results.
-    """
-    inputs = {
-        "topic": "AI LLMs"
-    }
-    try:
-        DockerCrewTemplate().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while replaying the crew: {e}")
-
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("No arguments provided. Defaulting to 'run' mode.")
-        run()
-    elif sys.argv[1] == "train":
-        train()
-    elif sys.argv[1] == "replay":
-        replay()
-    elif sys.argv[1] == "test":
-        test()
-    else:
-        print(f"Unknown command '{sys.argv[1]}'. Defaulting to 'run' mode.")
-        run()
-        
-        
+        raise Exception(f"An error occurred while testing the crew: {e}")
